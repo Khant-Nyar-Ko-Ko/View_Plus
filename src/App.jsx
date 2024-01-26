7c52dabb6a441ecdd4d1849f14bc29f09c5a0250
khantnyarkoko <kokhantnyar19@gmail.com>
2024-01-26 13:31:35 +0700

    tested footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 
@@ -6,6 +7,7 @@ const App = () => {
     <div>
       <Header/>
       <Hero/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div className=" w-full border-t-2">
+        <p className=" text-sm text-slate-300 w-1/2 mx-auto">@ 2024 Khant Nyar Ko Ko. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
